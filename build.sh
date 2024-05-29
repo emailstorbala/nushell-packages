117322ce7ff11479f288ddac823c9691da913d4f
anabarasan <nasarabna@gmail.com>
2024-05-29 19:37:08 +0530

    debian build script

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
-# delete any existing rpms
+# delete any existing packages
+echo "deleting existing rpm packages if any..."
 rm -f nushell-*.rpm
+
+echo "deleting existing deb packages if any..."
+rm -f nushell_*.deb
 
 for release in {39..41}; do
   docker run --rm -it -v $(pwd):/code fedora:${release} /code/fedora.sh
@@ -10,4 +14,8 @@ done
 
 for release in {8..9}; do
   docker run --rm -it -v $(pwd):/code rockylinux/rockylinux:${release} /code/rocky/build.sh
+done
+
+for release in "bookworm" "trixie"; do
+  docker run --rm -it -v $(pwd):/code debian:${release} /code/debian.sh
 done
